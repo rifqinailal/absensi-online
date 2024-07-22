29df408d968b3f07e9459fe00a5f750086ce0f1f
rifqinailal <rifqinailal@gmail.com>
2024-07-22 20:09:06 +0700

    frond end dashboard

---
+++ database/migrations/2024_07_22_124609_create_jadwals_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jadwals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->date('tanggal');
+            $table->string('shift');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jadwals');
+    }
+};
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,23 @@
 @extends('layout')
 @section('content')
 <div class="mx-3 my-3">
-    <h1 class="font-semibold text-3xl">Jadwal Piket</h1>
-    <div class="grid grid-cols-4 text-center border-b">
-        <h1>Nama</h1>
-        <h1>Hari</h1>
-        <h1>Tanggal</h1>
-        <h1>Shift</h1>
+    <h1 class="font-semibold text-3xl mt-5">Jadwal Piket</h1>
+    <div class="px-5 py-2 h-96">
+        <div class="grid grid-cols-4 text-center border-b mt-10 font-semibold">
+            <h1>Nama</h1>
+            <h1>Divisi</h1>
+            <h1>Tanggal</h1>
+            <h1>Shift</h1>
+        </div>
+        <div class="grid grid-cols-4 text-center mt-4">
+            <h1>Rifqi</h1>
+            <h1>Kwu</h1>
+            <h1>12/12/12</h1>
+            <h1>Pagi</h1>
+        </div>
     </div>
+   <div class="flex justify-end">
+    <a href="#"><button class="btn btn-primary rounded-xl w-36">Absen</button></a>
+   </div>
 </div>
 @endsection
